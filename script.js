a9201f8ba401b79444d65f6ea3deb9f5d00bbd92
shirin <shirinanlen@gmail.com>
2019-02-05 07:32:13 -0500

    canvas version 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('script conected!');
-
+//////////////
 ///timer///
-
+///////////
 //countdown from w3 tutorial https://www.w3schools.com/howto/howto_js_countdown.asp
 // let countDown = new Date("Jan 5, 2021 15:37:25").getTime();
 // let updateCount = setInterval(function() {
@@ -29,14 +29,16 @@ function startTimer(duration, display) {
  }, 1000);
 }
 window.onload = function () {
-    let twentySec = 10 * 2,
+    let thirtySec = 10 * 3,
         display = document.querySelector('#counter');
-    startTimer(twentySec, display);
+    startTimer(thirtySec, display);
 };
+
+
 /////
-
+///////////////////
 ////randomWords////
-
+//////////////
 let roulette = ['adult', 'animal', 'apple', 'art', 'artist', 'attorney', 'arm', 'American', 'article', 'bank', 'baby', 'bag', 'ball', 'bank', 'bar', 'black', 'blood', 'body', 'book', 'box', 'banana', 'boy', 'building', 'cancer', 'car', 'card', 'cell', 'center', 'chair', 'child', 'church', 'cold', 'computer', 'country', 'couple', 'crime', 'cup', 'cut', 'dark', 'dead', 'degree', 'dog', 'cat', 'door', 'down', 'draw', 'dream', 'drug', 'eat', 'eight', 'everybody', 'eye', 'face', 'family', 'father', 'film', 'finger', 'fire', 'first', 'fish', 'five', 'floor', 'follow', 'food', 'friend', 'game', 'garden', 'girl', 'gun', 'guy', 'hair', 'half', 'hand', 'happy', 'hear', 'heart', 'heavy', 'here', 'hit', 'hospital', 'image', 'key', 'kid', 'language', 'laugh', 'letter', 'light', 'list', 'long', 'live', 'love', 'low', 'machine', 'man', 'marriage', 'memory', 'money', 'morning', 'mouth', 'ear', 'movie', 'network', 'newspaper', 'night', 'nothing', 'number', 'ok', 'open', 'painting', 'paper', 'party', 'phone', 'piece', 'picture', 'plane', 'police', 'radio', 'room', 'sea', 'skull', 'somebody', 'size', 'song', 'star', 'moon', 'sun', 'black-hole', 'step','time', 'top', 'tree', 'flower', 'bee', 'cow', 'horse', 'monkey', 'zebra', 'TV', 'up', 'wait', 'wall', 'water', 'white', 'window', 'wish', 'woman', 'world', 'yes', 'no', 'elephant', 'giraffe' ]
 
 let start = document.querySelector('#btnReady');
@@ -45,7 +47,7 @@ let wordSpace = document.querySelector('#wordSpace');
 //my button is a null...trying to load the DOM before moving on..
 document.addEventListener('DOMContentLoaded', function() {
 start.addEventListener('click', (ev) => {
-  return start;
+  //return start;
   //debugger;
   //console.log('click');
   let randomWord = Math.floor(Math.random() * roulette.length + 1);
@@ -54,26 +56,64 @@ start.addEventListener('click', (ev) => {
 })
 })
 
-/////canvas////
+////////////////////////////////
+/////canvas//////////////
+//////////////////
+//tutorial from here: http://www.mattmorgante.com/technology/javascript-draw-html5-canvas
 
 let canvas = document.querySelector('#canvas');
 let context = canvas.getContext('2d');
-let height = canvas.height = window.innerHeight / 1.3; //double assign?
-let width = canvas.width = window.innerWidth / 2;
-let mouseClicked = false, mouseReleased = true;
-document.addEventListener('click', onMouseClick, false);
-document.addEventListener('mousemove', onMouseMove, false);
+canvas.height = window.innerHeight / 1.3; //double assign?
+canvas.width = window.innerWidth;
+context.lineJoin = 'round';
+context.lineCap = 'round';
+context.lineWidth = 10;
+context.strokeStyle = "#fff";
 
-function onMouseClick(e) {
-    mouseClicked = !mouseClicked;
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e){
+if(!isDrawing)return;
+console.log(e);
+context.beginPath();
+context.moveTo(e.offsetX, e.offsetY);
+context.lineTo(e.offsetX, e.offsetY);
+context.stroke();
+[lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
-function onMouseMove(e) {
-    if (mouseClicked) {
-        context.beginPath();
-        context.arc(e.clientX, e.clientY, 3.5, 0, Math.PI * 2, false);
-        context.lineWidth = 4;
-        context.fillStyle = "#fff";
-        context.fill();
-    }
-}
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+// this is from medium: https://medium.com/@jagadeshanh/html5-canvas-click-and-draw-f665e02f5744
+
+// let canvas = document.querySelector('#canvas');
+// let context = canvas.getContext('2d');
+// let height = canvas.height = window.innerHeight / 1.3; //double assign?
+// let width = canvas.width = window.innerWidth / 2;
+// let mouseClicked = false, mouseReleased = true;
+// document.addEventListener('click', onMouseClick, false);
+// document.addEventListener('mousemove', onMouseMove, false);
+//
+// function onMouseClick(e) {
+//     mouseClicked = !mouseClicked;
+// }
+//
+// function onMouseMove(e) {
+//     if (mouseClicked) {
+//         context.beginPath();
+//         context.moveTo(x,y);
+//         context.arc(e.clientX, e.clientY, 3.5, 0, Math.PI * 2, false);
+//         context.lineWidth = 4;
+//         context.fillStyle = "#fff";
+//         context.fill();
+//     }
+// }
