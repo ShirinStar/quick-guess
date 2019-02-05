9e662eb07075d03e877bdd9a1e037e9715b81329
shirin <shirinanlen@gmail.com>
2019-02-05 10:39:00 -0500

    keep working on canvas

--- script.js
+++ script.js
@@ -20,18 +20,19 @@ console.log('script conected!');
 function startTimer(duration, display) {
     let timer = duration, seconds;
     setInterval(function () {
-        seconds = parseInt(timer % 60, 10);
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = seconds;
-        if (--timer < 0) {
-         timer = duration;
-     }
- }, 1000);
+      seconds = parseInt(timer % 60, 10);
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      display.textContent = seconds;
+      if (--timer < 0) {
+       timer = duration;
+        //if(timer === 0) {
+      }
+    }, 1000);
 }
 window.onload = function () {
-    let thirtySec = 10 * 3,
-        display = document.querySelector('#counter');
-    startTimer(thirtySec, display);
+  let thirtySec = 10 * 3,
+  display = document.querySelector('#counter');
+  startTimer(thirtySec, display);
 };
 
 
@@ -41,20 +42,20 @@ window.onload = function () {
 //////////////
 let roulette = ['adult', 'animal', 'apple', 'art', 'artist', 'attorney', 'arm', 'American', 'article', 'bank', 'baby', 'bag', 'ball', 'bank', 'bar', 'black', 'blood', 'body', 'book', 'box', 'banana', 'boy', 'building', 'cancer', 'car', 'card', 'cell', 'center', 'chair', 'child', 'church', 'cold', 'computer', 'country', 'couple', 'crime', 'cup', 'cut', 'dark', 'dead', 'degree', 'dog', 'cat', 'door', 'down', 'draw', 'dream', 'drug', 'eat', 'eight', 'everybody', 'eye', 'face', 'family', 'father', 'film', 'finger', 'fire', 'first', 'fish', 'five', 'floor', 'follow', 'food', 'friend', 'game', 'garden', 'girl', 'gun', 'guy', 'hair', 'half', 'hand', 'happy', 'hear', 'heart', 'heavy', 'here', 'hit', 'hospital', 'image', 'key', 'kid', 'language', 'laugh', 'letter', 'light', 'list', 'long', 'live', 'love', 'low', 'machine', 'man', 'marriage', 'memory', 'money', 'morning', 'mouth', 'ear', 'movie', 'network', 'newspaper', 'night', 'nothing', 'number', 'ok', 'open', 'painting', 'paper', 'party', 'phone', 'piece', 'picture', 'plane', 'police', 'radio', 'room', 'sea', 'skull', 'somebody', 'size', 'song', 'star', 'moon', 'sun', 'black-hole', 'step','time', 'top', 'tree', 'flower', 'bee', 'cow', 'horse', 'monkey', 'zebra', 'TV', 'up', 'wait', 'wall', 'water', 'white', 'window', 'wish', 'woman', 'world', 'yes', 'no', 'elephant', 'giraffe' ]
 
-let start = document.querySelector('#btnReady');
-let wordSpace = document.querySelector('#wordSpace');
-
-//my button is a null...trying to load the DOM before moving on..
-document.addEventListener('DOMContentLoaded', function() {
-start.addEventListener('click', (ev) => {
-  //return start;
-  //debugger;
-  //console.log('click');
-  let randomWord = Math.floor(Math.random() * roulette.length + 1);
-  word = roulette[randomWord];
-  wordSpace.innerHTML = word;
-})
-})
+// let start = document.querySelector('#btnReady');
+// let wordSpace = document.querySelector('#wordSpace');
+//
+// //my button is a null...trying to load the DOM before moving on..
+// document.addEventListener('DOMContentLoaded', function() {
+//   start.addEventListener('click', (ev) => {
+//   //return start;
+//   //debugger;
+//   //console.log('click');
+//     let randomWord = Math.floor(Math.random() * roulette.length + 1);
+//     word = roulette[randomWord];
+//     wordSpace.innerHTML = word;
+//   })
+// })
 
 ////////////////////////////////
 /////canvas//////////////
@@ -76,12 +77,12 @@ let lastY = 0;
 
 function draw(e){
 if(!isDrawing)return;
-console.log(e);
-context.beginPath();
-context.moveTo(e.offsetX, e.offsetY);
-context.lineTo(e.offsetX, e.offsetY);
-context.stroke();
-[lastX, lastY] = [e.offsetX, e.offsetY];
+  console.log(e);
+  context.beginPath();
+  context.moveTo(e.offsetX, e.offsetY);
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 canvas.addEventListener('mousedown', (e) => {
