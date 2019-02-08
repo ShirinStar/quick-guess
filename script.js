5c912e4b6a24431cf22fd59b6fce9199776bfd16
shirin <shirinanlen@gmail.com>
2019-02-08 06:57:34 -0500

    making sure the final code is up

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// console.log('script conected!');
-
 const roulette = ['adult', 'animal', 'apple', 'art', 'artist', 'attorney', 'arm', 'American', 'article', 'bank', 'baby', 'bag', 'ball', 'bank', 'bar', 'black', 'blood', 'body', 'book', 'box', 'banana', 'boy', 'building', 'cancer', 'car', 'card', 'cell', 'center', 'chair', 'child', 'church', 'cold', 'computer', 'country', 'couple', 'crime', 'cup', 'cut', 'dark', 'dead', 'degree', 'dog', 'cat', 'door', 'down', 'draw', 'dream', 'drug', 'eat', 'eight', 'everybody', 'eye', 'face', 'family', 'father', 'film', 'finger', 'fire', 'first', 'fish', 'five', 'floor', 'follow', 'food', 'friend', 'game', 'garden', 'girl', 'gun', 'guy', 'hair', 'half', 'hand', 'happy', 'hear', 'heart', 'heavy', 'here', 'hit', 'hospital', 'image', 'key', 'kid', 'language', 'laugh', 'letter', 'light', 'list', 'long', 'live', 'love', 'low', 'machine', 'man', 'marriage', 'memory', 'money', 'morning', 'mouth', 'ear', 'movie', 'network', 'newspaper', 'night', 'nothing', 'number', 'ok', 'open', 'painting', 'paper', 'party', 'phone', 'piece', 'picture', 'plane', 'police', 'radio', 'room', 'sea', 'skull', 'somebody', 'size', 'song', 'star', 'moon', 'sun', 'black-hole', 'step','time', 'top', 'tree', 'flower', 'bee', 'cow', 'horse', 'monkey', 'zebra', 'TV', 'up', 'wait', 'wall', 'water', 'white', 'window', 'wish', 'woman', 'world', 'yes', 'no', 'elephant', 'giraffe', 'lion', 'kiss', 'holding-hands', 'coffee', 'breakfast', 'phone', 'tea', 'pen', 'train', 'robot', 'email', 'dance', 'teacher', 'floor-lamp', 'power-socket', 'strawberry', 'blueberry', 'pizza', 'french-fries', 'sky', 'harmony', 'street', 'sunday', 'rocket', 'superhero', 'hamburger', 'clock', 'naked', 'attention', 'nobody', 'shelter', 'trump', 'war', 'island', 'beach', 'football', 'mosquito', 'rat', 'trash', 'grapes', 'maybe', 'starting-point', 'button', 'box', 'empty', 'elevator', 'out-of-space', 'burrito', 'ramen-soup', 'headphones', 'park', 'homeless', 'cheese', 'tent', 'bed', 'birthday', 'font', 'text', 'work', 'internet']
 
 let currentWord;
@@ -32,7 +30,7 @@ threeC.style.display = "none";
 win.style.display = "none";
 lose.style.display = "none";
 
-  //button1 starting
+//button1 starting
 button1.addEventListener('click', function(){
   screenOne.style.display = "none";
   screenTwo.style.display = "block";
@@ -64,7 +62,6 @@ function counterView() {
 let timeLeft = 30;
 //////counter----- tutorial from https://stackoverflow.com/questions/4435776/simple-clock-that-counts-down-from-30-seconds-and-executes-a-function-afterward
 function countdown(counter) {
-  //console.log("counting")
   if (timeLeft === 0) {
     counter.innerHTML = timeLeft;
     clearTimeout(timerId);
@@ -94,7 +91,7 @@ button2.addEventListener('click', function(){
 let canvas = document.querySelector('#canvas');
   // could be 3d, if i want to make a video game
 let context = canvas.getContext('2d');
-// console.log(window.innerWidth);
+
 if (window.innerWidth < 721 ){
   canvas.width = 450
   canvas.height = 570
@@ -163,12 +160,6 @@ canvas.addEventListener('touchend', function(e){
   let mouseEvent = new MouseEvent('mouseup',{});
   canvas.dispatchEvent(mouseEvent);
 }, false)
-// function getTouchPos(canvasDom, touchEvent) {
-//   // let rect = canvasDom.getBoundingClientRect();
-//   return{
-//     x: touchEvent.touches[0].clientX-rect.left,
-// //     y: touchEvent.touches[0].clientY-rect.top
-// }
 //////end of Canvas //////
 
 //moving to second player!
@@ -228,7 +219,6 @@ input.addEventListener('keyup', function(e){
     }
   }
 })
-
 //refreshing
 buttonRoundWin.addEventListener('click', function() {
   screenOne.style.display = "block";
